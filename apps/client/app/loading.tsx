b8ee4700fddb7b2686d5dcad5dee7a7eafcf1496
edo92 <edoter92@gmail.com>
2023-11-08 01:10:17 -0800

    update client

--- apps/client/app/loading.tsx
+++ apps/client/app/loading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Loading from '@components/Loading';
-
+import {} from '@nx/workspace/src/tasks-runner/tasks-runner'
 const PageLoading: React.FC = () => {
   return <Loading />;
 };
